8a02d3368c1ac873c238956366e6d9d4207c0f35
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2024-03-18 19:19:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+  let ans = [];
+  let initialAns = []
+  let sum = 0;
+
+
+  for(let i=0 ; i<arr.length ; i++)
+  {
+    sum += arr[i];
+    // console.log(sum)
+    if(sum <= n){
+        initialAns.push(arr[i]);
+        console.log(initialAns)
+   }
+   else{
+    ans.push(initialAns);
+    initialAns=[];
+    sum = 0;
+    i--
+   }
+
+}
+(initialAns && ans.push(initialAns));
+
+return ans
 };
 
 const n = prompt("Enter n: ");
+// let n = 5;
+// console.log(divide(arr, n))
 alert(JSON.stringify(divide(arr, n)));
